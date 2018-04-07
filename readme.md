4d25b6fbcc2d701a212ba19f2dd0911b389eba9f
m4a1fox <bogdanovmaxim87@gmail.com>
2018-04-07 12:53:29 +0300

    First commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+## Begining:
+### Playtitle API package
---
+++ src/Playtitle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Playtitle;
+
+use GuzzleHttp;
+
+class Playtitle
+{
+    public function test()
+    {
+        echo '<pre>' . print_r(123321, 1) . '</pre>';
+        die;
+    }
+}
